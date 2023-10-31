ed33bdc3577cebbe97219171eeaaa2ca438528d1
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-10-31 19:48:28 +0530

    minor improvement in variable naming"

--- src/utils/useResList.js
+++ src/utils/useResList.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 const useResList = (resListUrl) => {
   const [resList, setResList] = useState(null);
 
-  const fetchResList = async (resListUrl) => {
-    const data = await fetch(resListUrl);
+  const fetchResList = async (url) => {
+    const data = await fetch(url);
     const jsonData = await data.json();
     setResList(
       jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
