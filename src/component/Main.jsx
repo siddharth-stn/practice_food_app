e28b83e40af0ef7ac9f2f75632f596cd89524772
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-11-01 18:04:59 +0530

    Some

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -6,40 +6,56 @@ import Shimmer from "./Shimmer";
 const Main = () => {
   const resList = useResList(RES_LIST_URL);
 
+  /*
+   * Upon clicking the Top Rated Restaurants button
+   * Find the restaurants having ratings 4 or more
+   * Then make useState() variable and store this list on this variable
+   * Now display the list of these restaurants in the UI
+   */
+
   if (resList === null) {
     return <Shimmer />;
   }
 
   return (
-    <div className="bg-yellow-300 pt-8 pb-6 px-32 flex flex-wrap gap-10">
-      {resList.map((res) => {
-        return (
-          <div
-            key={res?.info?.id}
-            className="p-4 rounded-[8px] bg-orange-500 [box-shadow:8px_8px_16px_#4c4848,_-8px_-8px_16px_#dacccc] text-white w-56"
-          >
-            <div className="flex flex-col gap-3">
-              <div className="">
-                <img
-                  src={CDN_URL + res?.info?.cloudinaryImageId}
-                  alt="restaurant pic"
-                />
+    <div className="bg-yellow-300 pt-8 pb-6 px-32 ">
+      <div>
+        <button className="bg-red-500 text-white p-1 px-3 rounded shadow-xl font-bold hover:border hover:border-solid hover:border-white hover:cursor-pointer">
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="pt-8 pb-6 flex flex-wrap gap-10">
+        {resList.map((res) => {
+          return (
+            <div
+              key={res?.info?.id}
+              className="p-4 rounded-[8px] bg-orange-500 [box-shadow:8px_8px_16px_#4c4848,_-8px_-8px_16px_#dacccc] text-white w-56"
+            >
+              <div className="flex flex-col gap-3">
+                <div className="">
+                  <img
+                    src={CDN_URL + res?.info?.cloudinaryImageId}
+                    alt="restaurant pic"
+                  />
+                </div>
+                <h3 className="font-bold text-black">
+                  Rating: {res?.info?.avgRating}
+                </h3>
+                <h2 className="font-bold text-black text-xl">
+                  {res?.info?.name}
+                </h2>
+                <h3 className="font-bold text-black">
+                  {res?.info?.cuisines.join(", ")}
+                </h3>
+                <h3 className="font-bold text-black">
+                  {res?.info?.costForTwo}
+                </h3>
               </div>
-              <h3 className="font-bold text-black">
-                Rating: {res?.info?.avgRating}
-              </h3>
-              <h2 className="font-bold text-black text-xl">
-                {res?.info?.name}
-              </h2>
-              <h3 className="font-bold text-black">
-                {res?.info?.cuisines.join(", ")}
-              </h3>
-              <h3 className="font-bold text-black">{res?.info?.costForTwo}</h3>
             </div>
-          </div>
-        );
-      })}
-      ;
+          );
+        })}
+        ;
+      </div>
     </div>
   );
 };
