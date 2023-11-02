ffdf2b0f8b5555e868c9626c7d891b835a041322
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-11-02 17:12:39 +0530

    s

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 
 const Main = () => {
   const [updatedRes, setUpdatedRes] = useState(null);
+  const [queryStr, setQueryStr] = useState(null);
 
   const resList = useResList(RES_LIST_URL);
 
@@ -21,13 +22,45 @@ const Main = () => {
     setUpdatedRes(filteredResList);
   };
 
+  /*
+   * When user inputs a letter
+   * then the function starts cheking the list of
+   * restaurants for a name of restaurant
+   * matching the query entered. If name matches the query then setUpdateRes() to the new list
+   */
+
+  const findQueryRes = (event) => {
+    setQueryStr(event.target.value);
+
+    if (queryStr != null) {
+      const someRes = updatedRes.filter((res) => {
+        return res?.info?.name.toLowerCase().includes(queryStr.toLowerCase());
+      });
+      console.log(someRes);
+      console.log(someRes.length);
+      if (someRes.length != 0) {
+        setUpdatedRes(someRes);
+      } else {
+        setUpdatedRes(resList);
+      }
+    }
+  };
+
   if (updatedRes === null) {
     return <Shimmer />;
   }
 
   return (
     <div className="bg-yellow-300 pt-8 pb-6 px-32 ">
-      <div>
+      <div className="flex items-center gap-10 ">
+        <div>
+          <input
+            type="text"
+            className="p-1 px-3 rounded border-red-500 border-2"
+            placeholder="I would eat at..."
+            onChange={findQueryRes}
+          />
+        </div>
         <button
           onClick={findTopRatedRes}
           className="bg-red-500 text-white p-1 px-3 rounded shadow-xl font-bold hover:border hover:border-solid hover:border-white hover:cursor-pointer"
@@ -65,7 +98,6 @@ const Main = () => {
             </div>
           );
         })}
-        ;
       </div>
     </div>
   );
